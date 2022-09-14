f7548f709d9f3b347c279bcf6642b50b4ece71d0
Amarjeet Mandal <amarjeet77019@gmail.com>
2022-09-14 19:56:55 +0530

    added condition

--- Code Library(Java)/3. Searching And Sorting/quickSortAlgo.java
+++ Code Library(Java)/3. Searching And Sorting/quickSortAlgo.java
@@ -1,13 +1,11 @@
-// time complexity ---average case O(nlogn)
-// time complexity ---worst case   O(n^2)
-
 package searchingAndSorting;
 
 import java.util.Scanner;
 
 public class quickSortAlgo {
 	
-	public static void swap(int arr[], int i , int j) {	
+	public static void swap(int arr[], int i , int j) {
+		
 		arr[i]= arr[i]^arr[j];
 		arr[j]= arr[i]^arr[j];
 		arr[i]= arr[i]^arr[j];		
@@ -15,35 +13,35 @@ public class quickSortAlgo {
 	     
 	public static int partition(int arr[], int l, int h) {
 		
-		int pivot = arr[l];  //first element as pivot
-		int i = l;           
+		int pivot = arr[l];  // first element as pivot
+		int i = l;
 		int j = h;
 		
 		while(i<j) {
 			
 			while(arr[i]<= pivot && i<h) 
-				i++;       // less than pivot or equal to pivot simply increment it
-			    
-			while(arr[j]>pivot && j>l) 
-				j--;       // yeh step tab hoga jab pivot se bada element milega aur start aur start ruk jayega, jab hume end ke sath khelna hai agar pivot se bada element mile to decrement karta rah, jab pivot se chota elemnet milega tab ruk jana hai.
+				i++;
 			
-			if(i < j)   // swap of start with end
+			while(arr[j]>pivot && j>l) 
+				j--;
+			
+			if(i < j)
 			  swap(arr,i,j);
 		
 		}
 		
 
-		if(j!=l)  // when they cross each other, swap pivot with end 
+		if(j>i) // or we can say like this also if(j!=l)
 			swap(arr,j,l );
 		
-		return j; // pivot element ka index mil gya
+		return j;
 		
 	}
 	
 	
 	public static void quickSort( int arr[], int left, int right) {
 		
-		if(left<right) { // don't forget this condition
+		if(left<right) {
 		
 		int pivotIndex = partition(arr, left , right);
 		quickSort(arr, left, pivotIndex-1);
@@ -61,6 +59,8 @@ public class quickSortAlgo {
 		
 		int arr[] = new int[size];
 		
+		
+		
 		System.out.println("enter the values in array");
 		
 		for(int i=0; i<size;i++) {
@@ -75,7 +75,7 @@ public class quickSortAlgo {
 		
 		quickSort(arr, left, right);
 				
-		// print the array
+		// print
 		
 		System.out.println("the sorted array is:");
 		
