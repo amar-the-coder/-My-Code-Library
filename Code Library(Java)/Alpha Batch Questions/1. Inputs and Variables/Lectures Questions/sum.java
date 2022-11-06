bedf65ccde8052ab97c56fcad7a80096904dbf27
Amarjeet Mandal <amarjeet77019@gmail.com>
2022-11-06 12:44:54 +0530

    added one folder

---
+++ Code Library(Java)/Alpha Batch Questions/1. Inputs and Variables/Lectures Questions/sum.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class sum {
+
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+        System.out.println("enter the value of a");
+        int a = s.nextInt();
+        System.out.println("enter the value of b");
+        int b = s.nextInt();
+
+        System.out.println("the sum of " + a + "+" + b + " is=" + (a + b));
+
+    }
+}
